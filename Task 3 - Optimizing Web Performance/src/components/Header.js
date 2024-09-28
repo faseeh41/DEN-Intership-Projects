7f2b0e10e6397008f8897a09f256e3473dedf6d7
faseeh41 <rajafaseeh41@gmail.com>
2024-09-28 12:57:38 +0500

    task-3

---
+++ Task 3 - Optimizing Web Performance/src/components/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header className="App-header">
+      <h1>Website Title</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
